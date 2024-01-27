65f6e8fe8401691e2d635cffca83d0fc9d9db3c5
venkathir <venkathir93@gmail.com>
2024-01-28 00:21:51 +0530

    New howe page added

---
+++ news-homepage-main/main.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  var navRightItems = document.querySelector(".navRightItems");
+  var hamburgerMenu = document.querySelector(".hamburger-menu");
+
+  navRightItems.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+}
